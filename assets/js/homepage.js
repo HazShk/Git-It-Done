46f1e8cd6757e5452e044227cd6646ed4bf09f84
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-15 19:53:46 -0400

    handle form submission, now the getuserrepos accepts a username

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,9 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function (user) {
   //format the github api url
+
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   //make a request to the url
@@ -9,4 +13,20 @@ var getUserRepos = function (user) {
     });
   });
 };
-getUserRepos("HazShk");
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+
+  //get value from input element
+  var userName = nameInputEl.value.trim();
+  if (userName) {
+    getUserRepos(userName);
+    //clear the input value
+    userName = "";
+  } else {
+    alert("Please enter a GitHub username");
+  }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
