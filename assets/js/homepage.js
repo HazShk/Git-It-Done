b689699b2015eaf77b36100838effe5adb442c98
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-17 11:31:41 -0400

    App Completed

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
   // prevent page from refreshing
@@ -18,6 +19,17 @@ var formSubmitHandler = function (event) {
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
+  }
+};
+
+var buttonClickHandler = function (event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+
+    //clear old content
+    repoContainerEl.textContent = "";
   }
 };
 
@@ -43,6 +55,25 @@ var getUserRepos = function (user) {
       //catching network errors
       window.alert("Unable to connect to Github");
     });
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+        console.log(data);
+      });
+    } else {
+      window.alert("Error; GitHub User Not Found");
+    }
+  });
 };
 
 //function to display repos
@@ -97,23 +128,6 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
-var getFeaturedRepos = function (language) {
-  var apiUrl =
-    "https://api.github.com/search/repositories?q=" +
-    language +
-    "+is:featured&sort=help-wanted-issues";
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-        console.log(data);
-      });
-    } else {
-      window.alert("Error; GitHub User Not Found");
-    }
-  });
-};
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
